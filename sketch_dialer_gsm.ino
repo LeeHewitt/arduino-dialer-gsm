e34f6382016aa80018c27b23eab7a19b31244e90
Lee Hewitt <lhtechnicalsolutions@icloud.com>
2014-11-07 19:59:54 +0000

    Update sketch_dialer_gsm.ino

--- sketch_dialer_gsm.ino
+++ sketch_dialer_gsm.ino
@@ -12,29 +12,29 @@
 
 #define PINNUMBER ""
 
-#define SMSNUMBER1 "0123456789"
+#define SMSNUMBER1 "078332328929"
 #define SMSNUMBER2 ""
 #define SMSNUMBER3 ""
 #define SMSNUMBER4 ""
 
-//en: msg booted
-#define SMSSTART "Antifurto: Avviato"
-//en: msg electric current blackout 
-#define SMSBLACKOUT "Antifurto: Attenzione corrente mancante"
-//en: msg electric current blackout off
-#define SMSBLACKOUTOFF "Antifurto: Corrente tornata"
-//en: msg intrusion attempt or triggered alarm
-#define SMSINTRUSION "Antifurto: Attenzione tentativo di furto"
+//en: initial boot message
+#define SMSSTART "LH Test Message"
+//en: low pressure message 
+#define SMS_LOW_PRESSURE "WARNING: Low Pressure"
+//en: high pressure message
+#define SMS_HIGH_PRESSURE "WARNING:High Pressure"
+//en: power failure message
+#define SMS_POWER_FAILURE "POWER FAILURE"
 
 GSM gsmAccess;
 GSM_SMS sms;
 boolean sendhttp = false;
 
-const int blackoutPin = 5;
-const int intrusionPin = 6;
+const int low_pressure = 5;
+const int high_pressure = 6;
 
-int blackout = 0;
-int intrusion = 0;
+int lowpressure = 0;
+int highpressure = 0;
 
 void setup()
 {
@@ -45,7 +45,7 @@ void setup()
     ; // wait for serial port to connect. Needed for Leonardo only
   }
 
-  Serial.println("SMS Messages Sender");
+  Serial.println("SMS Messages Sender...");
 
   // connection state
   boolean notConnected = true;
